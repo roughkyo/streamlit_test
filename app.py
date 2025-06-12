c05fad5741ea403977bdc5e74494461feffb3494
roughkyo <roughkyo@gmail.com>
2025-06-13 06:56:15 +0900

    4th

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.title('🖼️ yt-img-app')
 st.header('YouTube 썸네일 이미지 추출기 앱')
 
-with st.expander('이 앱에 대하여'):
+with st.expander('썸네일 추출기 app 사용법(클릭)'):
     st.write('이 앱은 YouTube 동영상의 썸네일 이미지를 추출해 제공함')
     st.write('사용법: 유튜브 주소가 https://www.youtube.com/watch?v=p2EGFTsXbyM&ab_channel=YTN 라면? ')
     st.write('✅ab_channel 전주소까지만 추출 ➡️ "https://youtu.be/p2EGFTsXbyM"  까지만 입력')
